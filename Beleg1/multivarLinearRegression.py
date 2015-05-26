bb2059d54e4a2ea36c7030525572cf7afdc52288
Simon A <simonh.arnold@gmail.com>
2015-05-26 10:28:47 +0200

    Some changes

--- Beleg1/multivarLinearRegression.py
+++ Beleg1/multivarLinearRegression.py
@@ -1,18 +1,31 @@
 #!/usr/bin/python
 
 import numpy as np
-import plotty, debug_printer
+import plotty
 import sys
 
+def get_debug_function(enabled=False):
+    def d(debugValues, caller=None):
+        debugHeader = "Debug message from {0} {1}".format
+        debugElements = "Element {n} of type {t} (Shape: {v.shape}):\n{v}".format
+        print( debugHeader(caller if caller else "None", 30*"-") )
+        print( "\n".join( [ debugElements(n=name, v=value, t=type(value)) for name, value in debugValues.iteritems() ] ) )
+    if not enabled:
+        # Return dummy function so no debug is printed
+        return lambda debugValues, caller=None: 1+1
+    else:
+        return d
 isDebug = (len(sys.argv) > 1 and "-d" in sys.argv)
-debug = debug_printer.get_debug_function(isDebug)
+debug = get_debug_function(isDebug)
+
 
 dataSets, features = 100,  2
-min_x, max_x = -10, 10
-original_thetas = thetas = np.array( [1, 2, 3] )
+min_x, max_x = -10., 10.
+original_thetas = np.array( [1, 2, 3] )
+thetas = np.array( [ -0.3, 3, 3.17 ] )
 y_noise_intensity = 2
-iterations = 10
-alpha = 0.00001
+iterations = 10000
+alpha = 0.001
 costs = {}
 
 ### Aufgabe 1
@@ -31,7 +44,7 @@ def vector_linear_hypothesis(thetaVector):
         return np.dot(xFeatureVector, thetaVector)
     return costFunc
 
-h = vector_linear_hypothesis(thetas)
+h = vector_linear_hypothesis(original_thetas)
 debug( {"Exakte Thetas" : original_thetas} )
 
 ###  Aufgabe 3
@@ -39,15 +52,11 @@ debug( {"Exakte Thetas" : original_thetas} )
 y = np.array( [ h(featureVector) for featureVector in xMatrix ] )
 debug( {"Y before noise" : y })
 # Add some noise to it
-y += np.random.randn(dataSets) * y_noise_intensity
+# y += np.random.randn(dataSets) * y_noise_intensity
 debug( {"Y noisy" : y })
 
 # Plot as a scatter plot
-# ax.scatter(xMatrix[:, 1], xMatrix[:, 2], y, "b", s=60, marker="*")
-scatterPlot = plotty.ScatterPlotty3D(xMatrix[:,1], xMatrix[:,2], y, "Randomly generated X features with calculated Y value")
-scatterPlot.setLabel("Feature 1", "Feature 2", "Generated  Y values")
-scatterPlot.show()
-# scatterPlot(xMatrix, y)
+plotty.scatter(xMatrix[:,1], xMatrix[:,2], y)
 
 def cost_function(xMatrix, y):
     m = len(xMatrix)
@@ -57,7 +66,7 @@ def cost_function(xMatrix, y):
         return 1. / ( 2. * m ) * ( loss ** 2 ).sum()
     return squared_error_cost
 
-j = cost_function(xMatrix, y)
+# j = cost_function(xMatrix, y)
 
 # Merke: theta.transpose() * xi == h_theta(x)
 def compute_new_theta(xMatrix, y, thetas, alpha):
@@ -67,14 +76,21 @@ def compute_new_theta(xMatrix, y, thetas, alpha):
     debug( {"New theta" : thetas_neu}, "In compute new theta" )
     return thetas_neu
 
-# Do 10.000 iterations
-for i in range(iterations):
-    thetas = compute_new_theta(xMatrix, y, thetas, alpha)
-    costs[i] = cost_function(xMatrix, y)(thetas)
+def gradient_decent(alpha, theta, nb_iterations, X, y):
+    # Do 10.000 iterations
+    for i in range(iterations):
+        theta = compute_new_theta(xMatrix, y, theta, alpha)
+        costs[i] = cost_function(xMatrix, y)(theta)
+    return (theta, costs)
+
+result_thetas, result_costs = gradient_decent(alpha, thetas, iterations, xMatrix, y)
+plotty.plot_costs(costs)
+# plotty.plot_result_plain(xMatrix[:,1], xMatrix[:,2], y, vector_linear_hypothesis(result_thetas))
+plotty.plot_result_plain(xMatrix[:,1], xMatrix[:,2], y, result_thetas)
 
 print("Done after {} iterations.".format(iterations))
 print("{0} I assume, that I have found something close to the original theta.{0}\n \
-        I guess it must have been around.. \n {1}".format( (30*"-"), np.vectorize("%.2f".__mod__)(thetas)))
+        I guess it must have been around.. \n {1}".format( (30*"-"), result_thetas))
 
 print("(Original thetas were {}".format(original_thetas))
 
--- Beleg1/plotty.py
+++ Beleg1/plotty.py
@@ -1,31 +1,45 @@
 #!/usr/bin/python
 
+import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 
-class Plotty:
+def scatter(x1, x2, y):
+    if ( x1.shape[0] != x2.shape[0] != y.shape[0]):
+        raise("Invalid dimensions of operand!")
 
-    def __init__(self, plotTitle, subplots=1):
-        self.fig = plt.figure(figsize=(11, 8), dpi=160)
-        self.title = plotTitle
-        self.axe = self.fig.add_subplot(111, projection='3d', label=plotTitle)
+    fig = plt.figure(figsize=(11, 8), dpi=160)
+    title = "Randomly generated X features with calculated Y value"
+    axe = fig.add_subplot(111, projection='3d', label=title)
+    axe.scatter( x1, x2, y, c="r", s=60 )
+    axe.set_xlabel("Feature 1")
+    axe.set_ylabel("Feature 2")
+    axe.set_zlabel("Generated  Y values")
+    plt.title(title)
+#     plt.show()
 
-    def show(self):
-        plt.title(self.title)
-        plt.show()
+def plot_costs(costs):
+    plt.figure()
+    plt.plot(costs.keys(), costs.values(), 'r-')
+    plt.xlabel('Iterations')
+    plt.ylabel('Square error cost')
+    plt.title('Decrease of error cost throughout the update iterations')
+#     plt.show()
 
-class ScatterPlotty3D(Plotty):
+def plot_result_plain(x1, x2, y, result_theta):
+    fig = plt.figure(figsize=(11, 8), dpi=160)
+    title = "Randomly generated X features with calculated Y value"
+    axe1 = fig.add_subplot(111, projection='3d', label=title)
 
-    def __init__( self, x, y, z, plotLabel, color="b", pointSize=60):
-        Plotty.__init__(self, plotLabel)
-        if ( x.shape[0] != y.shape[0] != z.shape[0]):
-            raise("Invalid dimensions of operand!")
-        self.axe.scatter( x, y, z, c=color, s=pointSize, marker="*" )
+    axe1.scatter( x1, x2, y, c="r", s=60 )
 
-    def setLabel(self, xLabel, yLabel, zLabel):
-        self.axe.set_xlabel(xLabel)
-        self.axe.set_ylabel(yLabel)
-        self.axe.set_zlabel(zLabel)
+    plainFunc = lambda x, y: result_theta[0] + result_theta[1]*x + result_theta[2]*y
+    plainX, plainY = np.meshgrid(range(-10, 10), range(-10, 10))
+    print "XYZ: {} {} {}".format(plainX, plainY, plainFunc(plainX, plainY))
+    print "Shapes: {} {} {}".format(plainX.shape, plainY.shape, plainFunc(plainX, plainY).shape)
+    axe1.plot_surface(plainX, plainY, plainFunc(plainX, plainY), cmap=cm.gray, linewidth=0 )
+    plt.show()
 
 # def scatterPlot2D( x, y ):
 #     if ( xMatrix.shape[0] != y.shape[0] ):
